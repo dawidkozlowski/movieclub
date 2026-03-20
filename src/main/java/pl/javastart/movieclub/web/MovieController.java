1b2c4d127c55aa1fb95384598175df720287727f
Dawid Kozlowski <dawid_kozlowski@o2.pl>
2026-03-20 08:51:50 +0100

    add 404 handling for missing movie

--- src/main/java/pl/javastart/movieclub/web/MovieController.java
+++ src/main/java/pl/javastart/movieclub/web/MovieController.java
@@ -1,9 +1,11 @@
 package pl.javastart.movieclub.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 import pl.javastart.movieclub.domain.movie.MovieService;
 import pl.javastart.movieclub.domain.movie.dto.MovieDto;
 
@@ -19,8 +21,10 @@ public class MovieController {
 
     @GetMapping("/film/{id}")
     public String getMovie(@PathVariable long id, Model model) {
-        Optional<MovieDto> optionalMovie = movieService.findMovieById(id);
-        optionalMovie.ifPresent(movie -> model.addAttribute("movie", movie));
+        MovieDto movie = movieService.findMovieById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Nie znaleziono filmu o id: " + id));
+        model.addAttribute("movie", movie);
+
         return "movie";
     }
 }
